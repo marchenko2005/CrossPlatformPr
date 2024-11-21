551dee0583b06eac040597e230583e13c97b6d09
Maria Marchenko <maria.dmit.rovna.slv@gmail.com>
2024-11-21 17:26:57 +0200

    fix vagrantfile

--- Lab4/MMarchenkoLib/provision-linux.sh
+++ Lab4/MMarchenkoLib/provision-linux.sh
@@ -1,5 +1,5 @@
 
-# Оновлення системи та встановлення .NET SDK
+
 sudo apt-get update
 sudo apt-get install -y wget apt-transport-https
 wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb
@@ -7,8 +7,6 @@ sudo dpkg -i packages-microsoft-prod.deb
 sudo apt-get update
 sudo apt-get install -y dotnet-sdk-8.0
 
-# Перехід до директорії проекту
 cd /vagrant
 
-# Запуск LAB1
 dotnet run --project App -- --lab lab1 --input MMarchenkoLib/LAB1/INPUT.TXT --output MMarchenkoLib/LAB1/OUTPUT.TXT
--- Lab4/MMarchenkoLib/provision-mac.sh
+++ Lab4/MMarchenkoLib/provision-mac.sh
@@ -1,9 +1,6 @@
 
-# Встановлення .NET SDK через Homebrew
 brew install --cask dotnet-sdk
 
-# Перехід до проекту
 cd /vagrant
 
-# Запуск LAB1
 dotnet run --project App -- --lab lab1 --input MMarchenkoLib/LAB1/INPUT.TXT --output MMarchenkoLib/LAB1/OUTPUT.TXT
